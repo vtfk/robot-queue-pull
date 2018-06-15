779a88cdc9f8ab99166db2bda65e583d37a396f2
Geir Gåsodden <geir.gasodden@pythonia.no>
2018-06-15 10:19:46 +0200

    Implements dotenv support (minor)

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 module.exports = {
   JWT_SECRET: process.env.JWT_SECRET || 'Louie Louie, oh no, I got to go Louie Louie, oh no, I got to go',
   JOBS_DIRECTORY_PATH: process.env.JOBS_DIRECTORY_PATH || 'test/directories/jobs',
@@ -6,7 +9,7 @@ module.exports = {
   AUTH_USERNAME: process.env.AUTH_USERNAME || false,
   AUTH_PASSWORD: process.env.AUTH_PASSWORD || false,
   QUEUE_NEXT_URL: process.env.QUEUE_NEXT_URL || 'https://example.com/api/queue/next',
-  QUEUE_DELETE_URL: process.env.QUEUE_DELETE_URL || 'https://example.com/api/queue',
+  QUEUE_DELETE_URL: process.env.QUEUE_DELETE_URL || false,
   PAPERTRAIL_HOSTNAME: process.env.PAPERTRAIL_HOSTNAME || 'robot-queue-pull',
   PAPERTRAIL_HOST: process.env.PAPERTRAIL_HOST || 'logs.papertrailapp.com',
   PAPERTRAIL_PORT: process.env.PAPERTRAIL_PORT || 12345

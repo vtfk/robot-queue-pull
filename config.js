7735a2df19a9df42a477e2b5e7d9fbf5fdcc01fe
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-01-21 11:34:03 +0100

    QUEUE_NEXT_URL renamed to QUEUE_URL

--- config.js
+++ config.js
@@ -8,8 +8,7 @@ module.exports = {
   CALLBACK_URL: process.env.CALLBACK_URL || false,
   AUTH_USERNAME: process.env.AUTH_USERNAME || false,
   AUTH_PASSWORD: process.env.AUTH_PASSWORD || false,
-  QUEUE_NEXT_URL: process.env.QUEUE_NEXT_URL || 'https://example.com/api/queue/next',
-  QUEUE_DELETE_URL: process.env.QUEUE_DELETE_URL || false,
+  QUEUE_URL: process.env.QUEUE_URL || 'https://example.com/api/queue/next',
   PAPERTRAIL_HOSTNAME: process.env.PAPERTRAIL_HOSTNAME || 'robot-queue-pull',
   PAPERTRAIL_HOST: process.env.PAPERTRAIL_HOST || 'logs.papertrailapp.com',
   PAPERTRAIL_PORT: process.env.PAPERTRAIL_PORT || 12345

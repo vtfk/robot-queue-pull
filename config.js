cbff2a327440a56d211f411d147030cfe62bc1a0
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-01-21 11:35:09 +0100

    Cleanup

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ module.exports = {
   JWT_SECRET: process.env.JWT_SECRET || 'Louie Louie, oh no, I got to go Louie Louie, oh no, I got to go',
   JOBS_DIRECTORY_PATH: process.env.JOBS_DIRECTORY_PATH || 'test/directories/jobs',
   COPIES_DIRECTORY_PATH: process.env.COPIES_DIRECTORY_PATH || 'test/directories/copies',
-  CALLBACK_URL: process.env.CALLBACK_URL || false,
   AUTH_USERNAME: process.env.AUTH_USERNAME || false,
   AUTH_PASSWORD: process.env.AUTH_PASSWORD || false,
   QUEUE_URL: process.env.QUEUE_URL || 'https://example.com/api/queue/next',
+  CALLBACK_URL: process.env.CALLBACK_URL || false,
   PAPERTRAIL_HOSTNAME: process.env.PAPERTRAIL_HOSTNAME || 'robot-queue-pull',
   PAPERTRAIL_HOST: process.env.PAPERTRAIL_HOST || 'logs.papertrailapp.com',
   PAPERTRAIL_PORT: process.env.PAPERTRAIL_PORT || 12345
